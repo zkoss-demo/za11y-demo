8cab58daa337ae786a3b2e88976f6f4db31ec11c
Robert Wenzel <robertwenzel@potix.com>
2020-10-26 16:38:07 +0800

    clean up / typos / formatting / styling

--- src/main/java/zk/demo/a11y/FeedbackForm.java
+++ src/main/java/zk/demo/a11y/FeedbackForm.java
@@ -1,22 +1,21 @@
 package zk.demo.a11y;
 
-public class FeedbackForm{
-	
-	public FeedbackForm() {
-		super();
-	}
+public class FeedbackForm {
 	private int pizzaCount;
 	private String comment;
-	
+
 	public int getPizzaCount() {
 		return pizzaCount;
 	}
+
 	public void setPizzaCount(int pizzaCount) {
 		this.pizzaCount = pizzaCount;
 	}
+
 	public String getComment() {
 		return comment;
 	}
+
 	public void setComment(String comment) {
 		this.comment = comment;
 	}
